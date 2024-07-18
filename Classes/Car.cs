473eb9ef9cf3690e61316363930c45705712e308
Robert Montgomery <boobyvxvbooby_gmail.com>
2024-07-17 21:11:06 -0500

    Added little flare/spice

---
+++ Classes/Car.cs
@@ -0,0 +1,13 @@
+namespace Classes;
+
+public class Car
+{
+    //Property to stroe the make of the car
+    public string Make { get; set; }
+    
+    //Property to store the model of the car
+    public string Model { get; set; }
+    
+    //Property to store the year of the car
+    public int Year { get; set; }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "BZ4X";
+            myCar.Year = 2024;
+            
+            Console.WriteLine($"Make: {myCar.Make}");
+            Console.WriteLine("That is a good Dealer!");
+            Console.WriteLine($"Model: {myCar.Model}");
+            Console.WriteLine("WOW! is that an EV car? nice!");
+            Console.WriteLine($"Year: {myCar.Year}");
+            Console.WriteLine("Holy smokes its brand new this year!");
         }
     }
 }
